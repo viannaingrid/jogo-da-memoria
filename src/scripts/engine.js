c90b9be06bc4754bcf9f7af0a69869689e322d82
Ingrid Vianna <ingrid.vianna1@outlook.com>
2024-11-20 00:00:29 -0300

    :construction: concluido JS

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -16,7 +16,7 @@ const emojis = [
 ];
 let openCards = [];
 
-let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
+let shuffleEmojis = emojis.sort(() => Math.random() - 0.5);
 
 for (let i = 0; i < emojis.length; i++) {
     let box = document.createElement("section");
@@ -27,20 +27,32 @@ for (let i = 0; i < emojis.length; i++) {
     const gameContainer = document.querySelector(".game");
     if (gameContainer) {
         gameContainer.appendChild(box);
-    } 
+    }
 }
 
 function handleClick() {
-    if(openCards.length < 2){
+    if (openCards.length < 2) {
         this.classList.add("boxOpen");
         openCards.push(this);
     }
 
-    if(openCards.length == 2){
+    if (openCards.length === 2) {
         setTimeout(checkMatch, 500);
     }
 }
 
 function checkMatch() {
+    if (openCards[0].innerHTML === openCards[1].innerHTML) {
+        openCards[0].classList.add("boxMatch");
+        openCards[1].classList.add("boxMatch");
+    } else {
+        openCards[0].classList.remove("boxOpen");
+        openCards[1].classList.remove("boxOpen");
+    }
 
+    openCards = [];
+
+    if (document.querySelectorAll(".boxMatch").length === emojis.length) {
+        alert("Você Venceu!");
+    }
 }
