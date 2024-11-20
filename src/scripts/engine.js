276cf0a788d52df1fb7dba02c2d82630864567f9
Ingrid Vianna <ingrid.vianna1@outlook.com>
2024-11-19 23:35:21 -0300

    :bricks: construindo o engine do jogo

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -22,9 +22,25 @@ for (let i = 0; i < emojis.length; i++) {
     let box = document.createElement("section");
     box.className = "item";
     box.innerHTML = shuffleEmojis[i];
+    box.onclick = handleClick;
 
     const gameContainer = document.querySelector(".game");
     if (gameContainer) {
         gameContainer.appendChild(box);
     } 
+}
+
+function handleClick() {
+    if(openCards.length < 2){
+        this.classList.add("boxOpen");
+        openCards.push(this);
+    }
+
+    if(openCards.length == 2){
+        setTimeout(checkMatch, 500)
+    }
+}
+
+function checkMatch() {
+    
 }
